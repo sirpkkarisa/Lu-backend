8b17151ca4af9ba806130cfc7344d37e13347acb
Pascal <sirpkkarisa001@gmail.com>
2020-10-28 17:13:24 +0300

    improvements

--- client/src/js/scripts.js
+++ client/src/js/scripts.js
@@ -3,7 +3,7 @@
     const getElemById = (id) => (document.getElementById(id));
     const createElem = (elem) => document.createElement(elem);
     const url = `${location.protocol}//${location.host}/`;
-    
+
     const formatDate = (dt) => {
             if (!dt) {
                 return '--';
@@ -184,7 +184,7 @@ if (localStorage.getItem('token')) {
         getElemById('comment-form').addEventListener('submit', (e) => {
             e.preventDefault();
             const comment = getElemById('comment-input').value.trim();
-            fetch(`${url}/articles/${id}/comment`, {
+            fetch(`${url}articles/${id}/comment`, {
                 method: 'POST',
                 headers: new Headers({
                     'Content-Type': 'application/json',
@@ -235,7 +235,7 @@ if (localStorage.getItem('token')) {
                 let article = getElemById('article').value;
                 let authorId = localStorage.getItem('userId');
 
-                fetch(`${url}/articles/${articleId}`, {
+                fetch(`${url}articles/${articleId}`, {
                     method: 'PATCH',
                     headers: new Headers({
                         'Authorization': `Bearer ${localStorage.getItem('token')}`,
